a2906aebf15d338cb4df07c2636fd6445a5e089c
Michał Górski <gorskimichal27@gmail.com>
2019-12-28 15:26:46 +0100

    update

--- config.py
+++ config.py
@@ -1,8 +1,16 @@
 import os
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-
 DATA_DIR = os.path.join(ROOT_DIR,"data")
-EEG_DIR = os.path.join(DATA_DIR,"EEG")
-IMG_DIR = os.path.join(DATA_DIR,"IMG")
+
+DATA_RAW_DIR = os.path.join(DATA_DIR,"raw")
+EEG_RAW_DIR = os.path.join(DATA_RAW_DIR,"EEG")
+IMG_RAW_DIR = os.path.join(DATA_RAW_DIR,"IMG")
+
+DATA_PREPROCESSED_DIR = os.path.join(DATA_DIR,"preprocessed")
+EEG_PREPROCESSED_DIR = os.path.join(DATA_PREPROCESSED_DIR,"EEG")
+IMG_PREPROCESSED_DIR = os.path.join(DATA_PREPROCESSED_DIR,"IMG")
+
+
+
 
