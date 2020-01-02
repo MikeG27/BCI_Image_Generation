76af32f52a489f6f90420c3ab4c11e88b553f83c
Michał Górski <gorskimichal27@gmail.com>
2020-01-02 21:07:48 +0100

    const name refactor

--- config.py
+++ config.py
@@ -1,24 +1,32 @@
 import os
 
+
+# Roots
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_DIR = os.path.join(ROOT_DIR,"data")
 
+# RAW
 DATA_RAW_DIR = os.path.join(DATA_DIR,"raw")
 
-IMG_RAW_DIR = os.path.join(DATA_RAW_DIR,"IMG")
-IMG_TRAIN_RAW_DIR = os.path.join(IMG_RAW_DIR,"train")
-IMG_TEST_RAW_DIR = os.path.join(IMG_RAW_DIR,"test")
+# RAW IMG
+RAW_IMG_DIR = os.path.join(DATA_RAW_DIR, "IMG")
+RAW_IMG_TRAIN_DIR = os.path.join(RAW_IMG_DIR, "train")
+RAW_IMG_TEST_DIR = os.path.join(RAW_IMG_DIR, "test")
 
+# RAW EEG
 EEG_RAW_DIR = os.path.join(DATA_RAW_DIR,"EEG")
 
+# PREPROCESSED
 DATA_PREPROCESSED_DIR = os.path.join(DATA_DIR,"preprocessed")
-IMG_PREPROCESSED_DIR = os.path.join(DATA_PREPROCESSED_DIR,"IMG")
-IMG_TRAIN_PREPROCESSED_DIR = os.path.join(IMG_PREPROCESSED_DIR,"train")
-IMG_TEST_PREPROCESSED_DIR = os.path.join(IMG_PREPROCESSED_DIR,"test")
 
-EEG_PREPROCESSED_DIR = os.path.join(DATA_PREPROCESSED_DIR,"EEG")
+# PREPROCESSED IMG
+PREPROCESSED_IMG_DIR = os.path.join(DATA_PREPROCESSED_DIR, "IMG")
+PREPROCESSED_IMG_TRAIN__DIR = os.path.join(PREPROCESSED_IMG_DIR, "train")
+PREPROCESSED_IMG_TEST__DIR = os.path.join(PREPROCESSED_IMG_DIR, "test")
+
+#PREPROCESSED EEG
+PREPROCESSED_EEG_DIR = os.path.join(DATA_PREPROCESSED_DIR, "EEG")
 
 
-# Zrobic metode do buildowania wszystkich folderow
 
 
