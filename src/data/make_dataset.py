41dca49f2f47a23f55b2117d8669c35dee151a54
Michał Górski <gorskimichal27@gmail.com>
2020-01-03 12:51:51 +0100

    add all functionality

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,43 +1,74 @@
 import os
+
 import cv2
-import matplotlib.pyplot as plt
-from tqdm import tqdm
+import numpy as np
 from keras.datasets import mnist
+from tqdm import tqdm
 
 import config
 
 
-def generate_image_dataset(X, y, output_dir, width=300, height=300, cmap="gray"):
+def generate_image_dataset(X,output_dir, width=300, height=300):
     """
     Generate 300x300 gray images from mnist dataset and extract images to train/test folder
     :param X: numpy images list
     :param y: numpy class list
     :param output_dir: output directory for generated images
     :return:
+
     """
 
-    if len(os.listdir(output_dir)) != 0 :
+    image_list = []
+
+    if len(os.listdir(output_dir)) != 0:
         print(f"Directory {output_dir} is not empty")
         return
 
     dim = (width, height)
     for i in tqdm(range(len(X))):
         numpy_img = X[i]
-        image_number = y[i]
         resized_img = cv2.resize(numpy_img, dim, interpolation=cv2.INTER_AREA)
-        plt.imsave(f"{output_dir}/image{i}_number{image_number}.png", resized_img, cmap="gray")
+        image_list.append(resized_img)
 
-def generate_image_csv():
-    pass
+    return np.array(image_list)
+
+
+def save_generated_array(array, output_dir, output_name):
+    """
+    Save array to a binary file in numpy
+
+    :param array: image array
+    :param output_dir:  output directory
+    :param output_name : name of the file
+
+    """
+    if not os.path.exists(output_dir):
+        os.mkdir(output_dir)
+
+    output_path = os.path.join(output_dir, output_name)
+    np.save(output_path, array)
+
+    print(f"[{output_name}] numpy array was saved into {output_path}\n")
+
 
 def main():
-    (x_train, y_train), (x_test, y_test) = mnist.load_data()
-    generate_image_dataset(x_train, y_train, output_dir=config.IMG_TRAIN_RAW_DIR)
-    generate_image_dataset(x_test, y_test, output_dir=config.IMG_TEST_RAW_DIR)
-
     """ Runs data processing scripts to turn raw data from (../raw) into
         cleaned data ready to be analyzed (saved in ../processed).
     """
+    # load mnist dataset
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+    # reshape dataset
+    x_train = generate_image_dataset(x_train,output_dir=config.RAW_IMG_TRAIN_DIR)
+    x_test = generate_image_dataset(x_test,output_dir=config.RAW_IMG_TEST_DIR)
+
+    # save dataset
+    print("Saving image data ....")
+    save_generated_array(x_train, config.PREPROCESSED_IMG_TRAIN_DIR, "X_train")
+    save_generated_array(y_train, config.PREPROCESSED_IMG_TRAIN_DIR, "y_train")
+    save_generated_array(x_test, config.PREPROCESSED_IMG_TEST_DIR, "X_test")
+    save_generated_array(y_test, config.PREPROCESSED_IMG_TEST_DIR, "y_test")
+
 
 if __name__ == '__main__':
     main()
