ee54e6e99ea10f1fc66893b7a8841a04f678fbb4
Michał Górski <gorskimichal27@gmail.com>
2020-01-10 18:50:56 +0100

    update path names

--- config.py
+++ config.py
@@ -6,25 +6,13 @@ ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_DIR = os.path.join(ROOT_DIR,"data")
 
 # RAW
-DATA_RAW_DIR = os.path.join(DATA_DIR,"raw")
-
-# RAW IMG
-RAW_IMG_DIR = os.path.join(DATA_RAW_DIR, "IMG")
-RAW_IMG_TRAIN_DIR = os.path.join(RAW_IMG_DIR, "train")
-RAW_IMG_TEST_DIR = os.path.join(RAW_IMG_DIR, "test")
-
-# RAW EEG
-EEG_RAW_DIR = os.path.join(DATA_RAW_DIR,"EEG")
+RAW_DIR = os.path.join(DATA_DIR,"raw")
+RAW_IMG_DIR = os.path.join(RAW_DIR, "IMG")
+RAW_EEG_DIR = os.path.join(RAW_DIR,"EEG")
 
 # PREPROCESSED
 DATA_PREPROCESSED_DIR = os.path.join(DATA_DIR,"preprocessed")
-
-# PREPROCESSED IMG
 PREPROCESSED_IMG_DIR = os.path.join(DATA_PREPROCESSED_DIR, "IMG")
-PREPROCESSED_IMG_TRAIN_DIR = os.path.join(PREPROCESSED_IMG_DIR, "train")
-PREPROCESSED_IMG_TEST_DIR = os.path.join(PREPROCESSED_IMG_DIR, "test")
-
-#PREPROCESSED EEG
 PREPROCESSED_EEG_DIR = os.path.join(DATA_PREPROCESSED_DIR, "EEG")
 
 
