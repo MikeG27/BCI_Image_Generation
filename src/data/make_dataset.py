9ca150726c07f933bd8a0b81346fc8ba6ad85b7a
Michał Górski <gorskimichal27@gmail.com>
2020-01-18 23:39:10 +0100

    add gdrive downloader

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,70 +1,16 @@
+from google_drive_downloader import GoogleDriveDownloader as gdd
+import config
 import os
 
-import cv2
-import numpy as np
-from keras.datasets import mnist
-from tqdm import tqdm
-
-import config
-
-
-def generate_image_dataset(X, width=300, height=300):
-    """
-    Generate 300x300 gray images from mnist dataset and extract images to train/test folder
-    :param X: numpy images list
-    :param y: numpy class list
-    :param output_dir: output directory for generated images
-    :return:
-
-    """
-
-    image_list = []
-
-    dim = (width, height)
-    for i in tqdm(range(len(X))):
-        numpy_img = X[i]
-        resized_img = cv2.resize(numpy_img, dim, interpolation=cv2.INTER_AREA)
-        image_list.append(resized_img)
-
-    return np.array(image_list)
+def download_file(file_id,dest_path,unzip=False):
+    gdd.download_file_from_google_drive(file_id=file_id,
+                                        dest_path=dest_path,
+                                        unzip=unzip,showsize=True,overwrite=True)
+    print("/ Data was downloaded ")
 
 
-def save_generated_array(array, output_dir, output_name):
-    """
-    Save array to a binary file in numpy
-
-    :param array: image array
-    :param output_dir:  output directory
-    :param output_name : name of the file
-
-    """
-    if not os.path.exists(output_dir):
-        os.mkdir(output_dir)
-
-    output_path = os.path.join(output_dir, output_name)
-    np.save(output_path, array)
-
-    print(f"[{output_name}] numpy array was saved into {output_path}\n")
-
-
-def main():
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
-    """
-    # load mnist dataset
-    (x_train, y_train), (x_test, y_test) = mnist.load_data()
-
-    # reshape dataset
-    x_train = generate_image_dataset(x_train,output_dir=config.RAW_IMG_TRAIN_DIR)
-    x_test = generate_image_dataset(x_test,output_dir=config.RAW_IMG_TEST_DIR)
-
-    # save dataset
-    print("Saving image data ....")
-    save_generated_array(x_train, config.PREPROCESSED_IMG_TRAIN_DIR, "X_train")
-    save_generated_array(y_train, config.PREPROCESSED_IMG_TRAIN_DIR, "y_train")
-    save_generated_array(x_test, config.PREPROCESSED_IMG_TEST_DIR, "X_test")
-    save_generated_array(y_test, config.PREPROCESSED_IMG_TEST_DIR, "y_test")
-
-
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    file_id = '1nvSTPTUv5r6bc7axUJcz6dKHUjsqdgjm'
+    dest_path = config.RAW_EEG_DIR
+    filepath=os.path.join(dest_path,"mnist-64s.csv")
+    download_file(file_id,filepath)
