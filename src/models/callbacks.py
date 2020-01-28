c43520b5670e10ea617b573ca0fe5f9bcaf6accf
= <gorskimichal27@gmail.com>
2020-01-28 13:16:15 +0100

    refactor

--- src/models/callbacks.py
+++ src/models/callbacks.py
@@ -1,13 +1,10 @@
 import datetime
-import os
+
 import tensorflow as tf
 from tensorflow.keras.callbacks import ModelCheckpoint
 from tensorflow.keras.callbacks import ReduceLROnPlateau
-from keras.callbacks import TensorBoard
-from keras import backend as K
-# custom
 
-from config import MODEL_TENSORBOARD_DIR,MODEL_CHECKPOINTER_DIR
+from config import MODEL_TENSORBOARD_DIR, MODEL_CHECKPOINTER_DIR
 
 # Initialize tensorboard
 log_dir = MODEL_TENSORBOARD_DIR + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
@@ -16,5 +13,5 @@ tensorboard = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
 # Initialize model checkpointer
 checkpoint = ModelCheckpoint(MODEL_CHECKPOINTER_DIR, monitor='val_loss', verbose=1, save_best_only=True)
 
-#reduce on plateu
-reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=20,verbose=1, mode='auto',min_lr=0.0000001)
+# reduce on plateu
+reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=20, verbose=1, mode='auto', min_lr=0.0000001)
