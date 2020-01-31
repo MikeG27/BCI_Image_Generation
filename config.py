43d7feb9e6a617d864fccf24ae4955743bf5dc26
Michał Górski <gorskimichal27@gmail.com>
2020-01-31 16:56:20 +0100

    change histogram name to pdf

--- config.py
+++ config.py
@@ -68,7 +68,7 @@ FIGURES_DIR = os.path.join(REPORTS_DIR,"figures")
 FIGURES_PLOT = os.path.join(FIGURES_DIR,"plot.png")
 FIGURES_BOXPLOT = os.path.join(FIGURES_DIR,"boxplot.png")
 FIGURES_DISTRIBUTIONS = os.path.join(FIGURES_DIR,"distributions.png")
-FIGURES_PDF = os.path.join(FIGURES_DIR,"histogram.png")
+FIGURES_PDF = os.path.join(FIGURES_DIR,"pdf.png")
 FIGURES_ECDF = os.path.join(FIGURES_DIR,"ecdf.png")
 FIGURES_CORR = os.path.join(FIGURES_DIR,"corr.png")
 
